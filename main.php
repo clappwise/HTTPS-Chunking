8367188c7e3c5f6d143fe0ee60c2ecbae3923ca9
clappwise <149500606+clappwise@users.noreply.github.com>
2023-10-31 20:49:21 +0300

    Update main.php
    SERVER SIDE CODE TO PUT TOGETHER CHUNKS

--- main.php
+++ main.php
@@ -1,108 +1,89 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $chunk = $_POST['chunk']; // Received chunk
-    $chunkNumber = $_POST['chunkNumber']; // Chunk number
-    $totalChunks = $_POST['totalChunks']; // Total number of chunks
-    $uniqueKey = $_POST['uniqueKey']; // Identifier for the file
-
+    
+    if (isset($_POST['chunk'])) {
+        $chunk = $_POST['chunk']; // Received chunk
+    } else {
+        $chunk = "";
+    }
+    
+    if (isset($_POST['chunkNumber'])) {
+        $chunkNumber = $_POST['chunkNumber']; // Chunk number
+    } else {
+        $chunkNumber = "";
+    }
+    
+    if (isset($_POST['totalChunks'])) {
+        $totalChunks = $_POST['totalChunks']; // Total number of chunks
+    } else {
+        $totalChunks = "";
+    }
+    
+    if (isset($_POST['uniqueKey'])) {
+        $uniqueKey = $_POST['uniqueKey']; // Identifier for the file
+    } else {
+        $uniqueKey = "";
+    }
+    
+    // Retrieve the file type (MIME type) sent from the client
+    if (isset($_POST['fileType'])) {
+        $fileType = $_POST['fileType'];
+    } else {
+        $fileType = "application/octet-stream"; // Default to a generic binary type
+    }
+    
     $tempUploadPath = 'temp_upload_directory/' . $uniqueKey; // Temporary folder
     $finalUploadPath = 'final_upload_directory/' . $uniqueKey; // Permanent folder
+    
+    // Create the permanent folder if it doesn't exist
+    if (!file_exists($finalUploadPath)) {
+        mkdir($finalUploadPath, 0777, true);
+    }
+    
+    if ($chunkNumber == $totalChunks) {
+    // All chunks received, assemble the file
+     // Determine the correct MIME type based on the file extension
+    $mimeTypes = [
+        'txt' => 'text/plain',
+        'jpg' => 'image/jpeg',
+        'png' => 'image/png',
+        'mp4' => 'video/mp4', // MIME type for video files
+        'zip' => 'application/zip', // MIME type for zip files
+        // Add more mappings as needed
+    ];
+    
+    $fileExtension = $filetype;  // Replace with the actual file extension
+    if (isset($mimeTypes[$fileExtension])) {
+        $mimeType = $mimeTypes[$fileExtension];
+        header('Content-Type: ' . $mimeType);
+    }
+    
+    $outputFile = fopen("$finalUploadPath/finalfile.ext", 'w');
 
-    if (!file_exists($tempUploadPath)) {
-        mkdir($tempUploadPath, 0777, true);
+    for ($i = 1; $i <= $totalChunks; $i++) {
+        $chunkData = file_get_contents("$tempUploadPath/$i");
+        fwrite($outputFile, $chunkData);
     }
 
-    file_put_contents("$tempUploadPath/$chunkNumber", $chunk);
+    fclose($outputFile);
 
-    // Check if all chunks have been received
-    if ($chunkNumber == $totalChunks) {
-        // All chunks received, assemble the file
-        $outputFile = fopen("$finalUploadPath", 'w');
+   
+    
+    // Send the assembled file to the client
+    readfile("$finalUploadPath/finalfile.ext");
+    
+    // Clean up temporary chunks
+    for ($i = 1; $i <= $totalChunks; $i++) {
+        unlink("$tempUploadPath/$i");
+    }
 
-        for ($i = 1; $i <= $totalChunks; $i++) {
-            $chunkData = file_get_contents("$tempUploadPath/$i");
-            fwrite($outputFile, $chunkData);
-        }
 
-        fclose($outputFile);
-
-        // Clean up temporary chunks
-        for ($i = 1; $i <= $totalChunks; $i++) {
-            unlink("$tempUploadPath/$i");
-        }
-
-        // File is now fully assembled and saved in $finalUploadPath
+        
+        // File is now fully assembled and saved in $finalUploadPath/finalfile.ext
         echo json_encode(['message' => 'Upload Complete']);
     } else {
+        file_put_contents("$tempUploadPath/$chunkNumber", $chunk);
         echo json_encode(['message' => 'Chunk Received']);
     }
 }
-
-? >
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Chunked File Upload</title>
-</head>
-<body>
-    <input type="file" id="fileInput">
-    <button id="uploadButton">Upload</button>
-    <progress id="progressBar" max="100" value="0"></progress>
-    <div id="status"></div>
-
-    <script>
-        const fileInput = document.getElementById('fileInput');
-        const uploadButton = document.getElementById('uploadButton');
-        const progressBar = document.getElementById('progressBar');
-        const status = document.getElementById('status');
-
-        uploadButton.addEventListener('click', () => {
-            const file = fileInput.files[0];
-            if (file) {
-                const chunkSize = 1024 * 1024; // 1MB chunks (adjust as needed)
-                const totalChunks = Math.ceil(file.size / chunkSize);
-                let currentChunk = 0;
-
-                function uploadChunk() {
-                    const start = currentChunk * chunkSize;
-                    const end = Math.min(start + chunkSize, file.size);
-                    const chunk = file.slice(start, end);
-
-                    const formData = new FormData();
-                    formData.append('chunk', chunk);
-                    formData.append('chunkNumber', currentChunk + 1);
-                    formData.append('totalChunks', totalChunks);
-                    formData.append('uniqueKey', 'your_unique_key'); // Replace with a unique identifier
-
-                    fetch('main.php', {
-                        method: 'POST',
-                        body: formData
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        currentChunk++;
-
-                        const progress = (currentChunk / totalChunks) * 100;
-                        progressBar.value = progress;
-                        status.textContent = `Uploading... ${progress.toFixed(2)}%`;
-
-                        if (currentChunk < totalChunks) {
-                            uploadChunk();
-                        } else {
-                            progressBar.value = 100;
-                            status.textContent = 'Upload Complete';
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Error:', error);
-                        status.textContent = 'Upload Failed';
-                    });
-                }
-
-                uploadChunk();
-            }
-        });
-    </script>
-</body>
-</html>
+?>
